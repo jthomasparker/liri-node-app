5451e84471b3e9d13794f7d682c742bbc25509ae
Josh Parker <jthomasparker@gmail.com>
2018-04-20 15:38:26 -0400

    final push. finished everything including log.txt

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # liri-node-app
-GT Coding Bootcamp Week 10 Homework: Nodejs console app
+GT Coding Bootcamp Week 10 Homework: Nodejs Console App
+
+--Uses nodejs to take in user input and fulfill the requests
+
+--Type "node liri.js <request>" in the console
+
+--Request types:
+    my-tweets (gets the last 20 tweets)
+    spotify-this-song "song" (gets spotify info about the requested song)
+    movie-this "move name" (gets omdb info about the requested movie)
+    do-what-it-says (uses whatever request is in random.txt)
+
+    **Note: the movie/song requests will work with or without quotations around the movie/song
+
+--All requests/results are logged in log.txt
--- liri.js
+++ liri.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
-var keys = require("./keys.js")
-var SpotifyClient = require("node-spotify-api")
-var TwitterClient = require("twitter")
-var request = require("request")
-var fs = require("fs")
-var spotify = new SpotifyClient(keys.spotify)
-var twitter = new TwitterClient(keys.twitter)
+var keys = require("./keys.js");
+var SpotifyClient = require("node-spotify-api");
+var TwitterClient = require("twitter");
+var request = require("request");
+var fs = require("fs");
+var spotify = new SpotifyClient(keys.spotify);
+var twitter = new TwitterClient(keys.twitter);
 
 
+// determines what function to run based on user input
 function processArgs(command, data){
     switch(command){
         case 'my-tweets':
@@ -27,101 +28,187 @@ function processArgs(command, data){
             break;
 
         default:
-            console.log("I don't recognize " + command + " or " + data)
+            console.log("I don't recognize '" + command + "'");
+            console.log("Try one of these commands:\n'my-tweets'\n'spotify-this-song'\
+            \n'movie-this'\n'do-what-it-says");
             break;
     }
 }
 
 
+// gets the last 20 tweets
 function getTweets(){
+    //begin new log entry
+    var logMessage = "\n*****************BEGIN LOG ENTRY*****************\n";
+    logMessage += new Date() + "\nResults of 'my-tweets':\n";
+    updateLog(logMessage);
+    // set parameters for api call
     var endpoint = "statuses/user_timeline"
     var params = {
         user_name: "joshparker712",
         count: 20
     }
-
+    // call api
     twitter.get(endpoint, params, function(error, results){
         if(!error){
-            console.log(" __________________________")
-            console.log("|                          |")
-            console.log("|  Your Twitter Stats:     |")
-            console.log("|    Followers: " + results[0].user.followers_count + "          |")
-            console.log("|    Friends: " + results[0].user.friends_count + "            |")
-            console.log("|    Favorites: " + results[0].user.favourites_count + "          |")
-            console.log("|__________________________|")
-            console.log("\nRecent Tweets:")
+            var output = " __________________________\n";
+            output += "|                          |\n";
+            output += "|  Your Twitter Stats:     |\n";
+            output += "|    Followers: " + results[0].user.followers_count + "          |\n";
+            output += "|    Friends: " + results[0].user.friends_count + "            |\n";
+            output += "|    Favorites: " + results[0].user.favourites_count + "          |\n";
+            output += "|__________________________|\n";
+            output += "\nRecent Tweets:\n";
+            console.log(output);
+            updateLog(output);
             
            for(var i in results){
-                console.log("------------------------------")
-                console.log(results[i].created_at)
-                console.log(results[i].text)
-                console.log("------------------------------\n")
-           }
+                output = "------------------------------\n";
+                output += results[i].created_at + "\n";
+                output += results[i].text + "\n";
+                output += "------------------------------\n";
+                console.log(output);
+                updateLog(output);
+           };
         } else {
-            console.log("Sorry, there was an error getting your tweets")
-        }
-    })
-}
+            console.log("Sorry, there was an error getting your tweets");
+            output = "ERROR: " + error;
+            updateLog(output);
+        };
+    });
+};
 
+
+// gets the songs from spotify
 function getSong(song){
     if(song === undefined){
-        song = "The Sign Ace of Base"
-    }
+        song = "The Sign Ace of Base";
+    };
+    // start new log entry
+    var logMessage = "\n*****************BEGIN LOG ENTRY*****************\n";
+    logMessage += new Date() + "\nResults of 'spotify-this-song' " + song + ":\n";
+    updateLog(logMessage);
+    // set api parameters
     var params = {
         type: 'track',
         query: song,
-        limit: 10
-    }
-
+        limit: 5
+    };
+    //call the api
     spotify.search(params, function(error, results){
         if(!error){
-          var tracks = results.tracks.items
-          console.log("\n------------------------------\n")
+          var tracks = results.tracks.items;
+          console.log("\n------------------------------\n");
             for(var i in tracks){
-                var artists = []
-                var previewUrl = tracks[i].preview_url
+                var output = '';
+                var artists = [];
+                var previewUrl = tracks[i].preview_url;
 
                 for(var j in tracks[i].artists){
-                    artists.push(" " + tracks[i].artists[j].name)
-                }
+                    artists.push(" " + tracks[i].artists[j].name);
+                };
                 if(previewUrl === null || previewUrl === "undefined"){
                     previewUrl = "Currently Unavailable"
-                }
-                console.log("Song: " + tracks[i].name)
-                console.log("Artist(s): " + artists)
-                console.log("Album: " + tracks[i].album.name)
-                console.log("Preview: " + previewUrl)
-                console.log("\n------------------------------\n")
-            }
+                };
+                output += "Song: " + tracks[i].name + "\n";
+                output += "Artist(s): " + artists + "\n";
+                output += "Album: " + tracks[i].album.name + "\n";
+                output += "Preview: " + previewUrl + "\n";
+                output += "\n------------------------------\n";
+                console.log(output);
+                updateLog(output);
+            };
         } else {
-            console.log("I'm sorry, there was an error getting that song", error)
-        }
-    })
+            console.log("I'm sorry, there was an error getting that song");
+            output = "Error: " + error;
+            updateLog(output);
+        };
+    });
+};
 
 
-}
-
+// gets the movie data from omdb
 function getMovie(movie){
-    console.log("Ok I'll get more info about", movie)
+    if(movie === undefined){
+        movie = "Mr. Nobody";
+    };
+    //start new log entry
+    var logMessage = "\n*****************BEGIN LOG ENTRY*****************\n";
+    logMessage += new Date() + "\nResults of 'movie-this' " + movie + ":\n";
+    updateLog(logMessage);
+    // set the api url
+    var url = "http://www.omdbapi.com/?apikey=trilogy&t=" + movie + "&type=movie&plot=full";
+    // call the api
+    request(url, function(error, response, results){
+        if(!error&& response.statusCode === 200){
+            var  parsedData = JSON.parse(results);
+            var output = "\n------------------------------\n";
+            output += "Title: " + parsedData.Title;
+            output += "\nDirector: " + parsedData.Director;
+            output += "\nYear: " + parsedData.Year;
+            output += "\nRated: " + parsedData.Rated;
+            output += "\nCountry: " + parsedData.Country;
+            output += "\nLanguage: " + parsedData.Language;
+            output += "\nIMDB Rating: " + parsedData.imdbRating;
+            output += "\nRotten Tomatoes Rating: " + parsedData.Ratings[1].Value;
+            output += "\nStarring: " + parsedData.Actors;
+            output += "\nPlot: \n" + parsedData.Plot;
+            output += "\n------------------------------\n";
+            console.log(output);
+           } else {
+               console.log("I'm sorry, there was an error getting that movie");
+               output = "ERROR: " + error;
+           };
+    updateLog(output);
+    });
+    
 }
 
+// uses random.txt to determine what to run
 function doIt(){
-    console.log("I'm doing it")
-}
+    // new log entry
+    var logMessage = "\n*****************BEGIN LOG ENTRY*****************\n";
+    logMessage += new Date() + "\nResults of 'do-what-it-says':\n";
+    logMessage += "Next Log Entry will be result of command contained in random.txt";
+    updateLog(logMessage);
+    //read random.txt and send the info to processArgs function
+    fs.readFile("random.txt", "utf8", function(error, data){
+        var arr = data.split(",");
+        var command = arr[0];
+        var requestData = arr[1];
+        processArgs(command, requestData);
+    });
+};
 
+
+// updates log.txt
+function updateLog(data){
+    var file = "log.txt";
+    fs.appendFile(file, data, function(err){
+        if(err){
+            console.log("Error writing to log file");
+        };
+    });
+};
+
+
+// takes in user input
 function main(){
     var command = process.argv[2];
-    var requestData = process.argv[3]
+    var requestData = process.argv[3];
+    // if the user doesn't use quotes around their song/movie, 
+    // get the rest of the args and stick them in quotes
     if(process.argv.length > 4){
         for(var i = 4; i < process.argv.length; i++){
-            requestData += " "
-            requestData += process.argv[i]
-        }
-    }
+            requestData += " ";
+            requestData += process.argv[i];
+        };
+    };
+    processArgs(command, requestData);
+};
 
-    processArgs(command, requestData)
-}
 
+// start the program
 main();
 
 
